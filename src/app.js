aaf6dcfa18789283e2bbd555bf8010db169ba45c
Devmoni <devmoni97@gmail.com>
2025-02-02 14:21:52 +0530

    Add: CORS support for cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
 const faqRoutes = require('./routes/faqRoutes');
 const sequelize = require('./config/database');
 
@@ -8,6 +10,10 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', faqRoutes);
